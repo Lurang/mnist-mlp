cdb3e9cce3e2e1702de7b2889eae08dc639ba57e
Lurang <qorrhaapfhd@naver.com>
2022-05-29 23:51:43 +0900

    feat/forward propagation

--- src/loss.ts
+++ src/loss.ts
@@ -1,7 +1,39 @@
+import { generateEmptyArray } from "./util";
+
 export function sigmoid(x: number) {
-    return 1 / (1 + Math.pow(Math.E, -x));
+    return 1 / (1 + Math.exp(-x));
 }
 
 export function dSigmoid(x: number) {
     return sigmoid(x) * (1 - sigmoid(x));
 }
+
+export function softmax(logits: number[]) {
+    const maxLogit = Math.max(...logits);
+    const scores = logits.map(l => Math.exp(l - maxLogit));
+    const denom = scores.reduce((a, b) => a + b);
+
+    return scores.map(s => s / denom);
+}
+
+export function predict(target: number) {
+    const arr = generateEmptyArray<number>(10).fill(0);
+    arr[target] = 1;
+
+    return arr
+}
+
+// const a = [
+//     0.7858382952908483,
+//     0.781120836787831,
+//     0.784249232385085,
+//     0.7753951278880614,
+//     0.7719111150678549,
+//     0.7736594971253697,
+//     0.7738955258572545,
+//     0.798528150282469,
+//     0.7696352118150711,
+//     0.7767945867782355
+// ];
+
+// console.log(softmax(a));
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import { trainImages } from './images';
+import { predict, sigmoid } from './loss';
 import { generateEmptyArray } from './util';
 
-const weight = generateEmptyArray<number>(28 * 28).fill(1);
+const HIDDEN_LAYER_NODE = 128;
+const OUTPUT_LAYER_NODE = 10;
 
-const netpj = trainImages[0].image.reduce((sum, current, index) => {
-    return sum + weight[index] * current;
-}, 0);
+const weight = generateEmptyArray<number>(HIDDEN_LAYER_NODE).map(_ =>
+    generateEmptyArray<number>(28 * 28).map(_ => Math.random() / 100)
+);
+const bios = generateEmptyArray<number>(HIDDEN_LAYER_NODE).map(_ =>
+    generateEmptyArray<number>(28 * 28).map(_ => Math.random() / 100)
+);
+
+const weight2 = generateEmptyArray<number>(OUTPUT_LAYER_NODE).map(_ =>
+    generateEmptyArray<number>(HIDDEN_LAYER_NODE).map(_ => Math.random() / 100)
+);
+const bios2 = generateEmptyArray<number>(OUTPUT_LAYER_NODE).map(_ =>
+    generateEmptyArray<number>(HIDDEN_LAYER_NODE).map(_ => Math.random() / 100)
+);
+
+for (const input of trainImages) {
+    const netpj = generateEmptyArray(HIDDEN_LAYER_NODE).map((_, i) => {
+        const totalVal = input.image.reduce((sum, current, j) => {
+            return sum + weight[i][j] * (current / 100000) + bios[i][j]; // downscale
+        }, 0);
+
+        return sigmoid(totalVal);
+    });
+
+    const netpk = generateEmptyArray(OUTPUT_LAYER_NODE).map((_, k) => {
+        const totalVal = netpj.reduce((sum, current, j) => {
+            return sum + current * weight2[k][j] + bios2[k][j];
+        }, 0);
+
+        return sigmoid(totalVal);
+    })
+
+    const target = predict(input.label);
+
+    console.log(netpk);
+}
+
+
+// normalize 255로 나눔
+
+// input은 784개 output은 10개
+
+// hidden 1 256개
+
+// hidden 2 128
